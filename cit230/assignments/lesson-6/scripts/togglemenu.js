0164c7db43943a6dbc3dce3fd7dbb3e1774b0e87
brandijacobs <cas15015@byui.edu>
2019-02-16 19:05:48 -0700

    update

--- cit230/assignments/lesson-6/scripts/togglemenu.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    //debugging: console.gof(document.getElementByID("primaryNav").classList);
-    document.getElementById("primaryNav").classList.toogle("hide");
-}
-
-var name = "Brandi Jacobs";
