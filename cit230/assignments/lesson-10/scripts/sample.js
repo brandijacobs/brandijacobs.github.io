15e44d7db7269761eaf49710b7fb80c8fa37dd54
brandijacobs <cas15015@byui.edu>
2019-03-16 16:15:47 -0600

    Saturday morning

---
+++ cit230/assignments/lesson-10/scripts/sample.js
@@ -0,0 +1,37 @@
+var person = { 
+    "name" : "Brother Thayne",
+    "location" : "Rexburg, ID"
+};
+console.log(person.name);
+document.write(person.location);
+
+// ------ Town Data ------
+var townData = {
+    // Paste town date
+};
+
+// can do, but not good practice
+// this or create placeholders line on line 34
+for (var i = 0; i < townData.length; i++) {
+    // two different approaches, choose
+    if (townData.towns[i].name === 'Soda Spring') {
+        document.write('<h1>' + townData.towns[i].name +'</h1>');
+
+        // must do the same for image
+        var imgSodaSprings = document.createElement('img');
+        imgSodaSprings.setAttribute("src", "images/");
+        document.body.appendChild(imgSodaSprings);
+    }
+    if (i === 0 || i === 2 | i === 3 || i === 6) {
+        continue;
+    }
+
+    document.write('<h1>' + townData.towns[1].name);
+}
+
+// Change values for aspects you want
+document.getElementById("townName1").innerHTML = townData.towns[1].name;
+document.getElementById("population1").innerHTML += townData.towns[1].currentPopulation;
+document.write(townData.towns[0].events[1]);
+
+// the easiest approach will be to add all the placeholders statically, and then add unknown elements through JSON
--- cit230/assignments/lesson-10/scripts/weatherapi.js
+++ cit230/assignments/lesson-10/scripts/weatherapi.js
@@ -1,4 +1,5 @@
-// var apiURLstring = (myappid - change)
+// Weather
+var apiURLstring = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a1e5e5a9c821a975cd2454f56d137abf&units=imperial';
 var weatherRequest = new XMLHttpRequest();
 weatherRequest.open('GET', apiURLstring, true);
 weatherRequest.responseType = 'json';
@@ -6,15 +7,30 @@ weatherRequest.send();
 
 weatherRequest.onload = function() {
     var weatherData = weatherRequest.response;
+    var temp = weatherData.main.temp;
+    var speed = weatherData.wind.speed;
 
-    // Temp
+    // error on pulling
+    document.getElementById("current-icon").setAttribute("src", 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png');
+    document.getElementById("current-temp").innerHTML = temp;
+    document.getElementById("current-pressure").innerHTML = weatherData.main.pressure;
+    document.getElementById("current-humidity").innerHTML = weatherData.main.humidity;
+    document.getElementById("current-wind").innerHTML = weatherData.wind.speed;
+    document.getElementById("current-chill").innerHTML = (35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)).toFixed(0);
+
     console.log(weatherData);
-
-    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
-   
-    // Get other required elements
-    document.getElementById("current-temp").innerHTML = weatherData.main.pressure;
-    document.getElementById("current-temp").innerHTML = weatherData.main.humidity;
-    document.getElementById("current-temp").innerHTML = weatherData.main.;
-    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
 }
+
+// Forecast
+// var apiForecaststring = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=a1e5e5a9c821a975cd2454f56d137abf&units=imperial';
+// var forecastRequest = new XMLHttpRequest();
+// weatherRequest.open('GET', apiForecaststring, true);
+// weatherRequest.responseType = 'json';
+// weatherRequest.send();
+
+// weatherRequest.onload = function() {
+//     var forecastData = weatherRequest.response;
+//     // console.log(forecastData);
+    // document.getElementById("day1").innerHTML = forecastData.
+
+// }
